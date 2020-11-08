fabf31c0fde7d2205c31e624911c1a49419b96cf
VILLERS Mickaël <mickael.villers@epitech.eu>
2020-11-08 20:29:55 +0100

    add service user

---
+++ services/user/src/main.ts
@@ -0,0 +1,21 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import {Transport} from "@nestjs/microservices";
+import {Logger} from "@nestjs/common";
+
+async function bootstrap() {
+  const port = process.env.PORT ? Number(process.env.PORT) : 4000;
+
+  const app = await NestFactory.create(AppModule);
+  app.connectMicroservice({
+    transport: Transport.TCP,
+    options: {
+      host: '0.0.0.0',
+      port: port
+    }
+  })
+
+  await app.startAllMicroservicesAsync();
+  await app.listen(port,() => Logger.log(`User microservice running on port ${port}`));
+}
+bootstrap();
