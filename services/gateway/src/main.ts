8f1cb4e151ee60042d6e12c809ee7aeb41ef1575
VILLERS Mickaël <mickael.villers@epitech.eu>
2020-11-14 15:42:15 +0100

    chore: add fastify

--- services/gateway/src/main.ts
+++ services/gateway/src/main.ts
@@ -1,12 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 
 const port = process.env.PORT ? Number(process.env.PORT) : 2000;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(port, () =>
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+  await app.listen(port, '0.0.0.0', () =>
     Logger.log(`Gateway microservice running on port ${port}`),
   );
 }
